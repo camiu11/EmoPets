79698fdb69e2d850646a846cc053da29203b433f
Camilo Torres <137632667+camiu11@users.noreply.github.com>
2024-05-03 20:58:01 -0500

    Add files via upload

---
+++ EmoPets.java
@@ -0,0 +1,8 @@
+package ws.emopets;
+
+public class EmoPets {
+
+    public static void main(String[] args) {
+        System.out.println("Y es que ser joven y no ser revolucionario es una contradicción hasta biológica");
+    }
+}
